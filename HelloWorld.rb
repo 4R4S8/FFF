bd472d8b18b9c19a1f19d24ca0ac5d193d38a219
4R4S8 <arashzomorrodi24@gmail.com>
2021-11-13 16:08:52 +0330

    first commit

---
+++ HelloWorld.rb
@@ -0,0 +1,25 @@
+x=5
+puts x
+puts x.class
+y=Float(x)
+puts y.class
+number1 = 2
+number2 = 3
+puts " #{number1+number2}"
+r=number1==number2
+puts r
+w=5000
+t=200
+puts z=w & t
+puts q = ~ 8
+for num in 1..8
+  puts num
+end  
+
+for numb in 1...8
+  puts numb
+end  
+
+for char in 'A'...'G'
+  puts char
+end  
