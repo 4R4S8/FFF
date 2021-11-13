39400af504d7b04dbbd2967284425b1c88f6b605
Arash <64580530+4R4S8@users.noreply.github.com>
2021-11-13 20:30:27 +0330

    First Try (part 2)

--- HelloWorld.rb
+++ HelloWorld.rb
@@ -1,25 +1,74 @@
-x=5
-puts x
-puts x.class
-y=Float(x)
-puts y.class
-number1 = 2
-number2 = 3
-puts " #{number1+number2}"
-r=number1==number2
-puts r
-w=5000
-t=200
-puts z=w & t
-puts q = ~ 8
-for num in 1..8
-  puts num
-end  
+choice = gets.to_i
 
-for numb in 1...8
-  puts numb
+case choice
+  when 1
+    puts "1"
+  when 2
+    puts "2"
+  else 
+    puts "else"
 end  
+puts choice.class
+i=1
+while i<=100
+  print " "
+  print i
+  i=i+1
+  
+end
 
-for char in 'A'...'G'
-  puts char
+for j in [1,5,9,8,9]
+  puts "Number #{j}"
 end  
+for t in 1..20
+  puts t
+end
+number =1
+begin 
+end  
+#PrintMessage
+  puts "Hello World"
+  #number +=1
+#end until number >10  
+
+myArray = [1,2,3,4,5]
+
+myArray.each do |number|
+  puts "#{number}"
+end
+
+u=[1,2,3,6,5]
+puts u
+
+def calculate_sum
+  c=2
+  v=5
+  sum=c+v
+  
+  return sum
+end
+
+result = calculate_sum
+
+puts result
+
+def varArguments(*args)
+  total = 0
+  
+  for number in args
+    total = total + number
+  end
+  
+  return total
+end
+
+puts varArguments(1,2,3,5)
+puts varArguments(1,5,3,6)
+
+show = lambda {print("Hello World")}
+
+show.call
+
+getsquare =lambda {|number|number*number}
+
+puts (getsquare.call(5))
